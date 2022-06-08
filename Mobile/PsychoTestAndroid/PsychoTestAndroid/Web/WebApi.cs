df00e816ceb360a34d6273c6becbcdb793b1dfe5
isiziakov <ivan.siziakov@mail.ru>
2022-06-08 15:35:18 +0300

    Testing httpClient

--- Mobile/PsychoTestAndroid/PsychoTestAndroid/Web/WebApi.cs
+++ Mobile/PsychoTestAndroid/PsychoTestAndroid/Web/WebApi.cs
@@ -9,6 +9,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
 
 namespace PsychoTestAndroid.Web
 {
@@ -31,5 +35,27 @@ namespace PsychoTestAndroid.Web
             //
             return tests;
         }
+
+        //public static async Task<List<Test>> apiForTests(string code) // тестирование возможности обращаться
+        //к апи, будет удалено после реализации функционала по обращению к апи
+        //{
+        //    List<Test> tests = new List<Test>();
+        //    string s = await GetTests();
+        //    return tests;
+        //}
+
+        //static async Task<string> GetTests()
+        //{
+        //    using (var client = new HttpClient())
+        //    {
+        //        client.BaseAddress = new Uri("https://cat-fact.herokuapp.com");
+        //        client.DefaultRequestHeaders.Accept.Clear();
+        //        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                
+        //        var result = await client.GetAsync("/facts");
+
+        //        return await result.Content.ReadAsStringAsync();
+        //    }
+        //}
     }
 }
